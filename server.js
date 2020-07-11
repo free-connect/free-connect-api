5239d6b20131c58add1d78fb2a5c3e02c795b452
mountainguy76935 <stoll33david@gmail.com>
2020-07-11 11:49:37 -0600

    cors fix

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
     res.set({
         'Content-Security-Policy': "script-src 'self'"
     })
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Origin', 'https://free-connect.herokuapp.com')
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
