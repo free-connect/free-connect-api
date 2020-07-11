3f932757133a43694fd1448ce8f60820b7fa5dcc
mountainguy76935 <stoll33david@gmail.com>
2020-07-11 13:35:24 -0600

    security

--- server.js
+++ server.js
@@ -45,6 +45,16 @@ const fileFilter = (req, file, cb) => {
     }
 }
 
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+      return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+  }
+
+app.use(requireHTTPS)
+
 app.use(bodyParser.json())
 
 app.use(multer({
