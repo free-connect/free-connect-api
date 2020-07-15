d251296986595a5dbc6c7af375d67475141afca5
mountainguy76935 <stoll33david@gmail.com>
2020-07-15 09:56:58 -0600

    some small fixes

--- server.js
+++ server.js
@@ -45,14 +45,14 @@ const fileFilter = (req, file, cb) => {
     }
 }
 
-function requireHTTPS(req, res, next) {
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-      return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-  }
+// function requireHTTPS(req, res, next) {
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+//       return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+//   }
 
-app.use(requireHTTPS)
+// app.use(requireHTTPS)
 
 app.use(bodyParser.json())
 
@@ -65,7 +65,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
     res.set({
-        'Content-Security-Policy': "script-src 'self'; frame-ancestors 'none'"
+        'Content-Security-Policy': "script-src 'self', frame-ancestors 'none'; X-Content-Type-Options 'nosniff'"
     })
     res.setHeader('Access-Control-Allow-Origin', process.env.APPROVED_URL)
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
